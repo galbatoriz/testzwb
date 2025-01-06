0b2e5bfa15b86604509ee568f249f26966129ee8
galbatoriz <65725180+galbatoriz@users.noreply.github.com>
2025-01-06 10:01:38 +0100

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -3,52 +3,42 @@
  */
 
 //% color="#AA278D"
-//% groups="['Drehen', 'Fahren']"
+//% groups="['Drehen', 'Fahren', 'Konfiguration',]"
 namespace TestMotion {
     //% block
     //% group="Drehen"
     export function turnLeft90() {
+
+    }
+
     //% block
     //% group="Drehen"
-    }
     export function turnRight90() {
-    //% block
-    //% group="Drehen"
+
     }
+
     //% block
     //% group="Drehen"
     export function turnDegrees() {
 
     }
+
     //% block
     //% group="Fahren"
-    export function driveForward() {
-    //% block
-    //% group="Fahren"
-    }
-    export function driveBackward() {
-    //% block
-    //% group="Fahren"
-    }
     export function driveDistance() {
+
+    }
     //% block
     //% group="Fahren"
-    }
     export function driveTime() {
 
     }
 
-    // note that Caml casing yields lower case
-    // block text with spaces
-
-
-
-    /**
-     * This block goes on top
-     */
     //% block
-    //% weight=100
-    export function first() {
+    //% group="Konfiguration"
+    export function setDistance() {
 
     }
+
+
 }
