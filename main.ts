b3b9dee761b3c8fc337126f3604d0e61a2416be2
galbatoriz <65725180+galbatoriz@users.noreply.github.com>
2025-01-06 09:32:36 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,30 @@
+namespace TestMotion {
+    //% block
+    export function helloWorld() {
+
+    }
+
+    // note that Caml casing yields lower case
+    // block text with spaces
+
+    //% block
+    export function camlCaseTwo() {
+
+    }
+    /**
+ * This block goes to the bottom
+ */
+    //% block
+    //% weight=50
+    export function second() {
+    }
+
+    /**
+     * This block goes on top
+     */
+    //% block
+    //% weight=100
+    export function first() {
+
+    }
+}
